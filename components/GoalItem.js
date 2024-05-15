0aa680f9cf59a08e7d87fc7f3026474195126763
nirgluzman <nir.gluzman@gmail.com>
2024-05-15 11:09:00 +0200

    update styling for press effect / delete item

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,13 +2,16 @@ import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 function GoalItem(props) {
   return (
-    // 'bind' is a JS method to pre-configure functions for future execution.
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      {/* In iOS we need to wrap <Text> with <View> component; that's not mandatory in Android. */}
-      <View style={styles.goalItem}>
+    // In iOS we need to wrap <Text> with <View> component; that's not mandatory in Android.
+    <View style={styles.goalItem}>
+      {/* 'bind' is a JS method to pre-configure functions for future execution. */}
+      <Pressable
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        // android_ripple={{ color: 'gray' }} // this property does not work in iOS.
+        style={({ pressed }) => pressed && styles.pressedItem}>
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -17,13 +20,17 @@ export default GoalItem;
 // A StyleSheet is an abstraction similar to CSS StyleSheets, https://reactnative.dev/docs/stylesheet
 const styles = StyleSheet.create({
   goalItem: {
-    margin: 8,
-    padding: 8,
+    margin: 8, // margin = whitespace available surrounding an element.
     borderRadius: 6,
     backgroundColor: '#5e0acc'
   },
 
   goalText: {
-    color: 'white'
+    color: 'white',
+    padding: 8 // padding = amount of inner space an element has.
+  },
+
+  pressedItem: {
+    opacity: 0.5
   }
 });
